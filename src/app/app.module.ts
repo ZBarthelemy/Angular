609dcb4382e01bde7a8aebd86de67a33992fffa6
ZBarthelemy <edward.a.barthelemy@gmail.com>
2019-01-02 18:55:38 +0900

    new angular form page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewsComponent } from './components/news/news.component';
-import { PageNotFoundComponent } from './page-not-found.component';
 import { FormComponent } from './components/form/form.component';
+
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { MatExpansionModule, MatFormFieldModule,
+         MatInputModule, MatSelectModule } from "@angular/material";
+
 @NgModule({
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatExpansionModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule
+  ],
   declarations: [
     AppComponent,
     HeaderComponent,
@@ -20,11 +36,6 @@ import { FormComponent } from './components/form/form.component';
     NewsComponent,
     PageNotFoundComponent,
     FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ErrorStateMatcher} from "@angular/material";
+import { FormControl, FormGroupDirective,
+         NgForm, Validators} from "@angular/forms";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
 
-  constructor() { }
+export class FormComponent implements OnInit {
+  options: string[] = ['Newbie', 'Proficient', 'Experience', 'Expert'];
+  panelOpenState = false;
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+  constructor() {
+  }
 
   ngOnInit() {
   }
